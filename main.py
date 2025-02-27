56ecbfd340734e96039e0f5beea9faf6bd602e30
acortinovis <adelio.cortinovis@gmail.com>
2025-02-27 12:29:33 -0800

    pseudocodes
    pseudocodes

---
+++ main.py
@@ -0,0 +1,12 @@
+#Create a program that will take user input about dinner guests they are inviting, add names to a list, and print out invitations for each dinner guest.
+
+#function to add names to list
+
+
+#function to print out invitations
+
+
+#ask the user if they want to change a guest
+
+
+#ask the user how many guests they want to invite

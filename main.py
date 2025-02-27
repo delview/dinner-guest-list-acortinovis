0c63d7cd00e6de9ae4851203cd0d26537d50983a
acortinovis <adelio.cortinovis@gmail.com>
2025-02-27 12:33:59 -0800

    function for list
    function to add guests to the list

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 #Create a program that will take user input about dinner guests they are inviting, add names to a list, and print out invitations for each dinner guest.
 
 #function to add names to list
-
-
+def add_guests(guest: str, number: int):
+    while True:
+        invited=[]
+        counter=0
+        if counter<number:
+            invited.append(guest)
+            counter+=1
+        else:
+            break
+        return invited
+    
 #function to print out invitations
 
 
